ea3bbbc99a616b7b0c3c6a43cfaed7fce8223473
jaytaytay <32568475+jaytaytay@users.noreply.github.com>
2020-03-08 10:12:41 +0800

    added find_packages() to setup.py to avoid needing to explicity list packages for flask app

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nous_app',
-    packages=['nous_app'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
